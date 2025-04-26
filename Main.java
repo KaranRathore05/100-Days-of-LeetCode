938b71ea9a12622a76ec55d44876b15e6792b828
Karangit config --global user.name Karan_Rathore05git config --global user.name Karan <karan0797s@gmail.com>
2025-04-26 22:52:52 +0530

    Day-13

---
+++ Main.java
@@ -0,0 +1,32 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, -2, 4};
+        System.out.println(maxProduct(nums));
+    }
+
+    public static int maxProduct(int[] nums) {
+        int maxSoFar = nums[0];
+        int maxEndingHere = nums[0];
+        int minEndingHere = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                int temp = maxEndingHere;
+                maxEndingHere = minEndingHere;
+                minEndingHere = temp;
+            }
+            
+            maxEndingHere = Math.max(nums[i], maxEndingHere * nums[i]);
+            minEndingHere = Math.min(nums[i], minEndingHere * nums[i]);
+            
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+        
+        return maxSoFar;
+    }
+}
+ 
+    
+

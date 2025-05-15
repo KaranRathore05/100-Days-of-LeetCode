b817a94721e95c48ccde770fa5b055b65d9d495b
Karangit config --global user.name Karan_Rathore05git config --global user.name Karan <karan0797s@gmail.com>
2025-05-15 22:07:47 +0530

    Day-29

---
+++ RemoveDuplicate.java
@@ -0,0 +1,25 @@
+public public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        String current = "";
+        int max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            String ch = String.valueOf(s.charAt(i));
+
+            if (current.contains(ch)) {
+                int index = current.indexOf(ch);
+                current = current.substring(index + 1);
+            }
+
+            current += ch;
+            if (current.length() > max) {
+                max = current.length();
+            }
+        }
+
+        return max;
+    }
+}
+ RemoveDuplicate {
+    
+}

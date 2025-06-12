d4c105dcba86f4bf502b12198a907986d4516b1b
Karangit config --global user.name Karan_Rathore05git config --global user.name Karan <karan0797s@gmail.com>
2025-06-12 23:17:26 +0530

    day - 46

---
+++ SpiralOrder.java
@@ -0,0 +1,52 @@
+
+import java.util.ArrayList;
+public class SpiralOrder {
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        int top = 0, bottom = matrix.length - 1;
+        int left = 0, right = matrix[0].length - 1;
+
+        while (top <= bottom && left <= right) {
+            // Top row
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+            }
+            top++;
+
+            // Right column
+            for (int i = top; i <= bottom; i++) {
+                res.add(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                // Bottom row
+                for (int i = right; i >= left; i--) {
+                    res.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                // Left column
+                for (int i = bottom; i >= top; i--) {
+                    res.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        List<Integer> result = sol.spiralOrder(matrix);
+        System.out.println(result);
+    }
+}

e6c79341478026723f85e3555c3ca2c0004ae00c
KARANPAL SINGH <karan0797s@gmail.com>
2025-07-08 23:03:29 +0530

    Day :54

---
+++ RemoveNthFromEnd.java
@@ -0,0 +1,50 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class RemoveNthFromEnd {
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode slow = head, fast = head;
+
+        for (int i = 1; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        if (fast == null) {
+            return head.next;
+        }
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        // Example: [1, 2, 3, 4, 5], remove 2nd from end → [1, 2, 3, 5]
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        int n = 2;
+
+        head = removeNthFromEnd(head, n);
+
+        // Print the resulting list
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+    }
+}

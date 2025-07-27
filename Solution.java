ec9100688924ba1c668520baa2d045d15e274df7
KARANPAL SINGH <karan0797s@gmail.com>
2025-07-27 07:49:07 +0530

    Day :59

---
+++ Solution.java
@@ -0,0 +1,21 @@
+public JumpGameII {
+    public int  jump(int []s) {
+    
+        int jump =0;
+        int currend =0;
+        int farthest =0;
+        for(int i = 0 ;i<s.length-1;i++){
+            int reach = s[i]+i;
+            if(reach > farthest){
+                farthest=reach;
+            }
+            if(i == currend){
+                jump++;
+                currend=farthest;
+            }
+        }
+        return jump;
+    }
+} {
+    
+}

9497e0fe0bb85b091bd9fd423df1bb94690b11fb
KARANPAL SINGH <karan0797s@gmail.com>
2025-07-28 22:00:12 +0530

    Day-64

--- RemoveNthFromEnd.java
+++ RemoveNthFromEnd.java
@@ -32,7 +32,7 @@ public class RemoveNthFromEnd {
         // Example: [1, 2, 3, 4, 5], remove 2nd from end → [1, 2, 3, 5]
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
+        head.next.next = ListNode(3);
         head.next.next.next = new ListNode(4);
         head.next.next.next.next = new ListNode(5);
 

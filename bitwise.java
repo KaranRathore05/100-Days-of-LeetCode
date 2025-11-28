865c15c1543d775eb8a8b96c86e0e311f8407d61
KARANPAL SINGH <karan0797s@gmail.com>
2025-11-28 12:26:32 +0530

    Day 92

---
+++ bitwise.java
@@ -0,0 +1,12 @@
+public class bitwise {
+    class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        final int n=bits.length;
+        int i=0;
+        while(i<n-1){
+            i+=1+bits[i];
+        }
+        return i==n-1;
+    }
+}
+}

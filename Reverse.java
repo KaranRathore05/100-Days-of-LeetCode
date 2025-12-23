3c0ec2b026846a23314bfb57523b257b3bdd145a
KARANPAL SINGH <karan0797s@gmail.com>
2025-12-23 11:16:55 +0530

    Day 100

---
+++ Reverse.java
@@ -0,0 +1,20 @@
+public class Reverse {
+    class Solution {
+    public int reverse(int x) {
+        int revnum = 0;
+
+        while (x != 0) {
+            int lastdi = x % 10;
+            x = x / 10;
+
+            if (revnum > Integer.MAX_VALUE / 10 || revnum < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+
+            revnum = revnum * 10 + lastdi;
+        }
+        return revnum;
+    }
+}
+~
+}
